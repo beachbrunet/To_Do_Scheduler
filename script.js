c1cd00152f6908b509dac6a246b9ea4cf85bb52d
Catherine Cox <ccox.034@gmail.com>
2021-08-14 19:53:04 -0600

    cleaned up the jargon for the storage

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
 // Need time blocks
 // Load the html and css files first then JS
-$(document).ready(function(){
+$(document).ready(function() {
     $("currentDay").text(moment().format("Do MMM YYYY, h:mm:ss a")); 
     
     
-    // save button
-$("saveBtn").on("click", function() {
-    let text = $(this).siblings(".disciption").val();
-    let time = $(this).parent().attr("id");
-
- 
+// save button
+    $("button").on("click", function() {
+     let text = $(this).siblings(".description").val();
+        localStorage.setItem(this.id,text);
+    })
+    
+    function load() {
+        $(this).siblings(".description").val(localStorage.getItem(this.id));
+    }
+    $("button").each(load)
 })
-   // Storing data by each hour
-   localStorage.setItem(time,text);
-
-   $("#9am.discription").val(localStorage.getItem("9am"));
-   $("#10am.discription").val(localStorage.getItem("10am"));
-   $("#11am.discription").val(localStorage.getItem("11am"));
-   $("#12pm.discription").val(localStorage.getItem("12pm"));
-   $("#1pm.discription").val(localStorage.getItem("1pm"));
-   $("#2pm.discription").val(localStorage.getItem("2pm"));
-   $("#3pm.discription").val(localStorage.getItem("3pm"));
-   $("#4pm.discription").val(localStorage.getItem("4pm"));
-   $("#5pm.discription").val(localStorage.getItem("5pm"));
-
-})
-}
-
-
-
-
-
